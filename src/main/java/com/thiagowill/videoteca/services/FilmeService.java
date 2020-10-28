cc369346081d85c0b38a31f1c15dad8558a799ca
ThiagoWill19 <thiagopompeu19@gmail.com>
2020-10-28 15:34:07 -0300

    alterada cor da navbar das páginas para preto

--- src/main/java/com/thiagowill/videoteca/services/FilmeService.java
+++ src/main/java/com/thiagowill/videoteca/services/FilmeService.java
@@ -1,12 +1,10 @@
 package com.thiagowill.videoteca.services;
 
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import com.thiagowill.videoteca.domain.Filme;
